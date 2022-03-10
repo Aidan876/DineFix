6193e0161cdf4c33ace9e3fbf9f15b451e245dbc
Aidan876 <100599080+Aidan876@users.noreply.github.com>
2022-03-10 21:57:18 +0000

    Files Upload

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,33 @@
+import test1 from "../assets/DineFix.png";
+import test2 from "../assets/DineFix.png";
+import test3 from "../assets/DineFix.png";
+import test4 from "../assets/DineFix.png";
+import test5 from "../assets/DineFix.png";
+import test6 from "../assets/DineFix.png";
+
+export const MenuList = [
+  {
+    name: "Placeholder1",
+    image: test1,
+  },
+  {
+    name: "Placeholder2",
+    image: test2,
+  },
+  {
+    name: "Placeholder3",
+    image: test3,
+  },
+  {
+    name: "Placeholder4",
+    image: test4,
+  },
+  {
+    name: "Placeholder5",
+    image: test5,
+  },
+  {
+    name: "Placeholder6",
+    image: test6,
+  },
+];
