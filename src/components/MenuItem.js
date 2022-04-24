2d3a1666d319e514adf6fd4dae72bb14240df358
007p-dotcom <73695040+007p-dotcom@users.noreply.github.com>
2022-04-24 14:20:19 +0100

    Add files via upload

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function MenuItem({ image, name, price }) {
+function MenuItem({ image, name, price, }) {
   return (
     <div className="menuItem">
       <div style={{ backgroundImage: `url(${image})` }}> </div>
       <h1> {name} </h1>
       <p> ${price} </p>
+      <button> add to cart </button>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/DineFix.png";
 import { Link } from "react-router-dom";
 import ReorderIcon from "@material-ui/icons/Reorder";
 import "../styles/Navbar.css";
@@ -13,19 +13,21 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} alt="logo" />
+        <img src={Logo} />
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
-          <Link to="/menu"> Restaurants </Link>
+          <Link to="/menu"> Menu </Link>
           <Link to="/about"> About </Link>
           <Link to="/contact"> Contact </Link>
+          <Link to="/review"> Review </Link>
         </div>
       </div>
       <div className="rightSide">
         <Link to="/"> Home </Link>
-        <Link to="/menu"> Restaurants </Link>
+        <Link to="/menu"> Menu </Link>
         <Link to="/about"> About </Link>
         <Link to="/contact"> Contact </Link>
+        <Link to="/review"> Review </Link>
         <button onClick={toggleNavbar}>
           <ReorderIcon />
         </button>
